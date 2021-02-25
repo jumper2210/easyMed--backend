2e479bd3a0b2ea1a5e3f4b21e162fb6d15cefb79
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2021-02-25 16:40:17 +0100

    roles: doctor

--- routes/medicine.js
+++ routes/medicine.js
@@ -1,28 +1,27 @@
-const express = require("express");
-const medicineController = require("../controllers/medicine");
-const router = express.Router();
-const isAuth = require("../middleware/is-auth");
-const { ROLES } = require("../helpers/_roles");
-const { authRole } = require("../helpers/_authRole");
+const express = require("express")
+const medicineController = require("../controllers/medicine")
+const router = express.Router()
+const isAuth = require("../middleware/is-auth")
+const { ROLES } = require("../helpers/_roles")
+const { authRole } = require("../helpers/_authRole")
 
 router.post(
   "/assignMedicine/:patientId",
   isAuth,
-  authRole(ROLES.DOCTOR),
   medicineController.assignMedicine
-);
+)
 
 router.get(
   "/loadPatientMedicines/:patientId",
   isAuth,
   medicineController.loadPatientMedicines
-);
+)
 
 router.delete(
   "/deleteMedicine/:medicineId",
   isAuth,
   authRole(ROLES.PATIENT),
   medicineController.deleteMedicine
-);
+)
 
-module.exports = router;
+module.exports = router
